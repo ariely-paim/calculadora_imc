4d221aff81d989b6f14548c91e8a3604cc5ce476
ariely-paim <arielyfloquet@gmail.com>
2023-10-05 17:10:13 -0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const form = document.getElementById('form');
 form.addEventListener('submit', function(event) {
-    /* Previne o comportamento padrão do evento submit do JS, ou seja,
-    impede o recarregamento da página */
     event.preventDefault();
 
     const weight = document.getElementById('weight').value;
